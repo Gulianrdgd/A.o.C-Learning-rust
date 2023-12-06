1a8ec3e3cd16cbeaad16c14fc27c9f9b4c042fa8
Julian van der Horst <julianvanderhorst+github@pm.me>
2023-12-06 11:12:11 +0100

    Day 6 done

--- day6/src/bin/part1.rs
+++ day6/src/bin/part1.rs
@@ -4,9 +4,25 @@ fn main() {
     println!("{}", result);
 }
 
-
 fn part1(input: &str) -> i64 {
+    let times = input.lines().nth(0).unwrap().split(":").nth(1).unwrap().split_whitespace().collect::<Vec<&str>>().iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let distance = input.lines().nth(1).unwrap().split(":").nth(1).unwrap().split_whitespace().collect::<Vec<&str>>().iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
 
+    let mut result = 1;
 
-    return 0;
+    for i in 0..times.len(){
+        let mut press_time = 0;
+        let mut wins = 0;
+
+        while press_time <= times[i]{
+            let reached_dist = (times[i] - press_time) * press_time;
+            if reached_dist > distance[i]{
+                wins +=1;
+            }
+            press_time += 1;
+        }
+        println!("wins: {}", wins);
+        result *= wins;
+    }
+    return result;
 }
--- day6/src/bin/part2.rs
+++ day6/src/bin/part2.rs
@@ -4,9 +4,25 @@ fn main() {
     println!("{}", result);
 }
 
-
 fn part2(input: &str) -> i64 {
-   
+    let times = input.lines().nth(0).unwrap().split(":").nth(1).unwrap().replace(" ", "").parse::<i64>().unwrap();
+    let distance = input.lines().nth(1).unwrap().split(":").nth(1).unwrap().replace(" ", "").parse::<i64>().unwrap();
 
-    return 0;
+    let mut result = 1;
+
+    let mut press_time = 0;
+    let mut wins = 0;
+
+    while press_time <= times{
+        let reached_dist = (times - press_time) * press_time;
+        if reached_dist > distance{
+            wins +=1;
+        }
+
+        press_time += 1;
+    }
+
+    result *= wins;
+
+    return result;
 }

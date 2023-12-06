96db90e97b11663d6b1a224783c406c3818d39f2
Julian van der Horst <julianvanderhorst+github@pm.me>
2023-12-06 09:41:38 +0100

    Ready for day 6

---
+++ day6/src/bin/part1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let input  = include_str!("./input1.txt");
+    let result = part1(input);
+    println!("{}", result);
+}
+
+
+fn part1(input: &str) -> i64 {
+
+
+    return 0;
+}
---
+++ day6/src/bin/part2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let input  = include_str!("./input2.txt");
+    let result = part2(input);
+    println!("{}", result);
+}
+
+
+fn part2(input: &str) -> i64 {
+   
+
+    return 0;
+}
